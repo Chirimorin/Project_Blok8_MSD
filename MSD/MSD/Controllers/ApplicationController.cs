c9557d5abb0fcf9c14b2d5ed5860741ca8b59a34
Thomas Voogt <chirimorin@gmail.com>
2013-04-29 13:50:34 +0200

    Basis opzet

---
+++ MSD/MSD/Controllers/ApplicationController.cs
@@ -0,0 +1,23 @@
+﻿using MSD.Services;
+using MSD.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSD.Controllers
+{
+    class ApplicationController : IApplicationController
+    {
+        private MainWindowService _mainWindowService;
+        private MainWindow _mainWindow;
+
+        public ApplicationController()
+        {
+            _mainWindow = new MainWindow();
+            _mainWindowService = new MainWindowService(_mainWindow);
+        }
+
+    }
+}
---
+++ MSD/MSD/Services/MainWindowService.cs
@@ -0,0 +1,20 @@
+﻿using MSD.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSD.Services
+{
+    class MainWindowService
+    {
+        private Views.MainWindow _mainWindow;
+
+        public MainWindowService(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+            _mainWindow.Show();
+        }
+    }
+}
