798e1b0c68b64a5b6368a227e6313f8d12223592
jeroen de graaff <jeroen_degraaff@hotmail.com>
2013-05-09 15:58:35 +0200

    stage details view en succes view

---
+++ MSD/MSD/Views/MatchSucces .xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MSD
+{
+	/// <summary>
+	/// Interaction logic for MatchSucces_.xaml
+	/// </summary>
+	public partial class MatchSucces_ : UserControl
+	{
+		public MatchSucces_()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
