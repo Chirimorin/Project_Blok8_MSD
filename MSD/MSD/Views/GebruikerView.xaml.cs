d08934c339fdb07c248c2ac541f851fa9098c367
Stefanie van de Poel <sjpoel@avans.nl>
2013-05-09 18:21:33 +0200

    alle view

---
+++ MSD/MSD/Views/GebruikerView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MSD
+{
+	/// <summary>
+	/// Interaction logic for GebruikerView.xaml
+	/// </summary>
+	public partial class GebruikerView : UserControl
+	{
+		public GebruikerView()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
---
+++ MSD/MSD/Views/StudentPersoonView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MSD
+{
+	/// <summary>
+	/// Interaction logic for StudentPersoonView.xaml
+	/// </summary>
+	public partial class StudentPersoonView : UserControl
+	{
+		public StudentPersoonView()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
