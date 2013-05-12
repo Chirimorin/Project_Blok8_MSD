2c3a75f4d8d9983ca1d8eec648d7ea72e585ce96
Stefanie van de Poel <sjpoel@avans.nl>
2013-05-12 17:08:03 +0200

    databinding

--- MSD/MSD/ViewModels/LogInViewModel.cs
+++ MSD/MSD/ViewModels/LogInViewModel.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using MSD.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace MSD.ViewModels
 {
     class LogInViewModel
     {
+        private MainWindow mainwindow = new MainWindow();
+
+
+        public MainWindow login
+        {
+            get { return mainwindow; }
+        }
+
     }
 }
--- MSD/MSD/Views/LogInView.xaml.cs
+++ MSD/MSD/Views/LogInView.xaml.cs
@@ -24,18 +24,5 @@ namespace MSD
 			
 			// Insert code required on object creation below this point.
 		}
-
-        private void WachtwoordVergeten(object sender, RoutedEventArgs e)
-        {
-            //open de wachtwoordvergetenusercontrol in de loginview
-        }
-
-        private void LogIn(object sender, RoutedEventArgs e)
-        {
-            MainWindow mainwindow = new MainWindow();
-            mainwindow.Show();
-            this.Close();
-
-        }
 	}
 }
