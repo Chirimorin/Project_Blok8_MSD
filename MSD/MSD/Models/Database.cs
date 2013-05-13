86ee28af3ac399aa104962fd5ef32fd9f0b8efda
Elmar de Vries <e.d.v766@gmail.com>
2013-05-13 18:05:06 +0200

    Database Connection gemaakt

---
+++ MSD/MSD/Models/Database.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace MSD.Models
+{
+    class Database
+    {
+        MySqlConnection myConnection = new MySqlConnection("Server=databases.aii.avans.nl;" + "Database=eavries_db2;" + "Uid=eavries;" + "Pwd=rd4qAS7j;");
+
+        public Database()
+        {
+            sqlConnection();
+        }
+
+        public void sqlConnection()
+        {
+            try
+            {
+                myConnection.Open();
+
+                MySqlCommand myCommand = new MySqlCommand("SELECT * FROM docent", myConnection);
+
+                MySqlDataReader myReader = myCommand.ExecuteReader();
+
+                while (myReader.Read())
+                {
+                    MessageBox.Show(myReader[0].ToString());
+                }
+
+                myConnection.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
