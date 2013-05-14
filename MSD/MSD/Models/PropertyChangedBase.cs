bdd77ab671dbcf42343be65703ca0e4cd558a944
Stefanie van de Poel <sjpoel@avans.nl>
2013-05-14 19:49:03 +0200

    relaycommand+propertychangedbase
    klasses gevult

--- MSD/MSD/Models/PropertyChangedBase.cs
+++ MSD/MSD/Models/PropertyChangedBase.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MSD.Models
 {
-    class PropertyChangedBase
+    class PropertyChangedBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
     }
+
 }
