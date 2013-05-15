c14ad130cd00a85d0c12f8c291edd9404c49d8d6
Stefanie van de Poel <sjpoel@avans.nl>
2013-05-15 11:53:32 +0200

    menuview in mainwindow

--- MSD/MSD/ViewModels/MainWindowModel.cs
+++ MSD/MSD/ViewModels/MainWindowModel.cs
@@ -1,4 +1,5 @@
-﻿using MSD.Models;
+﻿using MSD.Controllers;
+using MSD.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,36 @@ namespace MSD.ViewModels
     class MainWindowModel : PropertyChangedBase
     {
 
+        private readonly IApplicationController _app;
+        private readonly RelayCommand _ShowDocentCommand;
+        private readonly RelayCommand _ShowStudentCommand;
+        private readonly RelayCommand _ShowMatchCommand;
+        private readonly RelayCommand _ShowStageCommand;
+        private readonly RelayCommand _ShowGebruikersCommand;
+
         private PropertyChangedBase content;
+
+        public MainWindowModel(IApplicationController app)
+        {
+            _app = app;
+            this._ShowDocentCommand = new RelayCommand(ShowDocent);
+        }
+        
+        public RelayCommand ShowDocentCommand { get { return _ShowDocentCommand; } }
+        public void ShowDocent(object command)
+        {
+            _app.ShowDocentView();
+        }
+        public void ShowStudent(object command)
+        {
+            _app.ShowStudentView();
+        }
+
+        //username komt uit de database
+        public string UserName
+        {
+            get { return UserName; }
+        }
 
         public PropertyChangedBase Content
         {
