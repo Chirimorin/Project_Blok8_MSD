f1a8909738626e163291e5bb8f4281ab854d8017
Stefanie van de Poel <sjpoel@avans.nl>
2013-05-17 17:22:39 +0200

    bugfix

--- MSD/MSD/ViewModels/StudentPersoonViewModel.cs
+++ MSD/MSD/ViewModels/StudentPersoonViewModel.cs
@@ -11,10 +11,17 @@ namespace MSD.ViewModels
     class StudentPersoonViewModel : PropertyChangedBase
     {
         private readonly IApplicationController _app;
+        private readonly RelayCommand _verderCommand;
 
         public StudentPersoonViewModel(IApplicationController app)
         {
             _app = app;
+            _verderCommand = new RelayCommand(Verder);
+        }
+        public RelayCommand VerderCommand { get { return _verderCommand; } }
+        public void Verder(object command)
+        {
+            _app.ShowStudentBedrijfView();
         }
     }
 }
