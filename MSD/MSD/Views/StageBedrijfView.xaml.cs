b570044dc1a47a8b2d23b2c8c90d8ffbaf304ab4
jeroen de graaff <jeroen_degraaff@hotmail.com>
2013-05-22 10:34:39 +0200

    StageBedrijfView

---
+++ MSD/MSD/Views/StageBedrijfView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MSD
+{
+	/// <summary>
+	/// Interaction logic for StageBedrijfView.xaml
+	/// </summary>
+	public partial class StageBedrijfView : UserControl
+	{
+		public StageBedrijfView()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
