97f36834420c48c2ecb2b28b59a7c36db565220f
Thomas Voogt <chirimorin@gmail.com>
2013-05-22 20:36:25 +0200

    StageopdrachtViewModel

---
+++ MSD/MSD/ViewModels/StageopdrachtViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MSD.Controllers;
+using MSD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSD.ViewModels
+{
+    class StageopdrachtViewModel : PropertyChangedBase
+    {
+        private readonly IApplicationController _app;
+        private readonly RelayCommand _saveCommand;
+
+        public StageopdrachtViewModel(IApplicationController app)
+        {
+            _app = app;
+            _saveCommand = new RelayCommand(Save);
+        }
+
+        public RelayCommand SaveCommand { get { return _saveCommand; } }
+        public void Save(object command)
+        {
+            _app.ShowStudentView();
+        }
+    }
+}
