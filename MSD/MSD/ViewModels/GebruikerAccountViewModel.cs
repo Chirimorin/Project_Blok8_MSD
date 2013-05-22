696cdc52f0091c9c7b852a29bb010b6f3fe2757a
Thomas Voogt <chirimorin@gmail.com>
2013-05-22 13:42:59 +0200

    Terug knoppen gebruiker views

--- MSD/MSD/ViewModels/GebruikerAccountViewModel.cs
+++ MSD/MSD/ViewModels/GebruikerAccountViewModel.cs
@@ -12,17 +12,25 @@ namespace MSD.ViewModels
     {
         private readonly IApplicationController _app;
         private readonly RelayCommand _verderCommand;
+        private readonly RelayCommand _terugCommand;
 
         public GebruikerAccountViewModel(IApplicationController app)
         {
             _app = app;
             _verderCommand = new RelayCommand(Verder);
+            _terugCommand = new RelayCommand(Terug);
         }
 
         public RelayCommand VerderCommand { get { return _verderCommand; } }
         public void Verder(object command)
         {
             _app.ShowGebruikerContactView();
+        }
+
+        public RelayCommand TerugCommand { get { return _terugCommand; } }
+        public void Terug(object command)
+        {
+            _app.ShowGebruikerView();
         }
     }
 }
--- MSD/MSD/ViewModels/GebruikerContactViewModel.cs
+++ MSD/MSD/ViewModels/GebruikerContactViewModel.cs
@@ -12,17 +12,25 @@ namespace MSD.ViewModels
     {
         private readonly IApplicationController _app;
         private readonly RelayCommand _opslaanCommand;
+        private readonly RelayCommand _terugCommand;
 
         public GebruikerContactViewModel(IApplicationController app)
         {
             _app = app;
             _opslaanCommand = new RelayCommand(Opslaan);
+            _terugCommand = new RelayCommand(Terug);
         }
 
         public RelayCommand OpslaanCommand { get { return _opslaanCommand; } }
         public void Opslaan(object command)
         {
             _app.ShowGebruikerView();
+        }
+
+        public RelayCommand TerugCommand { get { return _terugCommand; } }
+        public void Terug(object command)
+        {
+            _app.ShowGebruikerAccountView();
         }
     }
 }
