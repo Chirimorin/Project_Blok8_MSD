2365fc02d74cc8c63641926f1d929805c7041962
Thomas Voogt <chirimorin@gmail.com>
2013-05-22 21:11:42 +0200

    Terug knop StudentPersoonView

--- MSD/MSD/ViewModels/StudentPersoonViewModel.cs
+++ MSD/MSD/ViewModels/StudentPersoonViewModel.cs
@@ -12,12 +12,21 @@ namespace MSD.ViewModels
     {
         private readonly IApplicationController _app;
         private readonly RelayCommand _verderCommand;
+        private readonly RelayCommand _terugCommand;
 
         public StudentPersoonViewModel(IApplicationController app)
         {
             _app = app;
             _verderCommand = new RelayCommand(Verder);
+            _terugCommand = new RelayCommand(Back);
         }
+
+        public RelayCommand TerugCommand { get { return _terugCommand; } }
+        public void Back(object command)
+        {
+            _app.ShowStudentView();
+        }
+
         public RelayCommand VerderCommand { get { return _verderCommand; } }
         public void Verder(object command)
         {
