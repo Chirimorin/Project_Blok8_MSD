036560cea37308adf19c1de057af6346058b82e3
Stefanie van de Poel <sjpoel@avans.nl>
2013-05-27 15:46:39 +0200

    schermen correct maken

--- MSD/MSD/ViewModels/GebruikerAccountViewModel.cs
+++ MSD/MSD/ViewModels/GebruikerAccountViewModel.cs
@@ -11,20 +11,20 @@ namespace MSD.ViewModels
     class GebruikerAccountViewModel : PropertyChangedBase
     {
         private readonly IApplicationController _app;
-        private readonly RelayCommand _verderCommand;
+        private readonly RelayCommand _opslaanCommand;
         private readonly RelayCommand _terugCommand;
 
         public GebruikerAccountViewModel(IApplicationController app)
         {
             _app = app;
-            _verderCommand = new RelayCommand(Verder);
+            _opslaanCommand = new RelayCommand(Opslaan);
             _terugCommand = new RelayCommand(Terug);
         }
 
-        public RelayCommand VerderCommand { get { return _verderCommand; } }
-        public void Verder(object command)
+        public RelayCommand VerderCommand { get { return _opslaanCommand; } }
+        public void Opslaan(object command)
         {
-            _app.ShowGebruikerContactView();
+            //opslaan van gegevens
         }
 
         public RelayCommand TerugCommand { get { return _terugCommand; } }
