101a6ca536867e28a7fdb901ed1de7ee6d96e16f
leon van woezik <leonvanwoezik@hotmail.com>
2013-06-26 12:55:15 +0200

    Je kan niet meer periode Alle kiezen bij stageopdracht

--- MSD/MSD/ViewModels/StageopdrachtViewModel.cs
+++ MSD/MSD/ViewModels/StageopdrachtViewModel.cs
@@ -170,7 +170,7 @@ namespace MSD.ViewModels
             {
                 Company[RowNr] = table.Rows[RowNr][0].ToString();
             }
-            cmd = new MySqlCommand("SELECT periodenaam FROM periode");
+            cmd = new MySqlCommand("SELECT periodenaam FROM periode where periodenaam <> 'Alle' ");
             table = new DataTable();
             adapter = ModelFactory.Database.getData(cmd);
             adapter.Fill(table);
